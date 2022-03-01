31958f489f5f19d544ddf82ba7e3d44626a74219
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-03-01 21:18:01 +0600

    responsive done

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,5 @@
 const main = document.getElementById('main');
+main.classList.add('test');
 
 const searchButton = () => {
     const input = document.getElementById('input-value');
@@ -9,7 +10,6 @@ const searchButton = () => {
         fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
             .then(res => res.json())
             .then(data => phoneDisplay(data.data))
-
         input.value = '';
         error.innerHTML = '';
     } else {
@@ -19,13 +19,17 @@ const searchButton = () => {
     }
 };
 const phoneDisplay = (phones) => {
+    if (phones.length == 0) {
+        error.innerText = `No phone found! 
+        Try another`;
+    }
     for (const phone of phones) {
-        // console.log(phone.image);
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
         div.classList.add('mb-5');
+        div.classList.add('element-center');
         div.innerHTML = `
-        <div class="card" style="width: 18rem;">
+        <div class="card shadow-lg p-3" style="width: 18rem;">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
@@ -40,21 +44,36 @@ const phoneDisplay = (phones) => {
     }
 };
 const phoneDetails = (slug) => {
+    console.log(slug);
     fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
         .then(res => res.json())
         .then(data => {
             const allPhones = data.data;
             const div = document.createElement('div');
+            div.classList.add('element-center');
             main.innerHTML = '';
             div.innerHTML = `
-                <div class="card" style="width: 18rem;">
+                <div class="card shadow-lg p-3" style="width: 18rem;">
                     <img src="${allPhones.image}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">${allPhones.name}</h5>
+                        <h5 class="card-title text-center">${allPhones.name}</h5>
                         <p>${allPhones.releaseDate}</p>
+                        <p><span class="fw-bold">Brand:</span> ${allPhones.brand}</p>
+                        <p><span class="fw-bold">Chipset:</span> ${allPhones.mainFeatures.chipSet}</p>
+                        <p><span class="fw-bold">Display Size:</span> ${allPhones.mainFeatures.displaySize}</p>
+                        <p><span class="fw-bold">Memory:</span> ${allPhones.mainFeatures.memory}</p>
+                        <p><span class="fw-bold">Others:</span></p>
+                        <ul>
+                            <li><span class="fw-bold">Bluetooth:</span> ${allPhones.others.Bluetooth}</li>
+                            <li><span class="fw-bold">GPS:</span> ${allPhones.others.GPS}</li>
+                            <li><span class="fw-bold">NFC:</span> ${allPhones.others.NFC}</li>
+                            <li><span class="fw-bold">Radio:</span> ${allPhones.others.Radio}</li>
+                            <li><span class="fw-bold">USB:</span> ${allPhones.others.USB}</li>
+                            <li><span class="fw-bold">WLAN:</span> ${allPhones.others.WLAN}</li>
+                        </ul>
                     </div>
                 </div>
             `;
             main.appendChild(div);
         })
-}
+};

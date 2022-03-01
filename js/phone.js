e66be7da21719223e4e7c1a115fc73a39f6984d1
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-03-01 14:28:27 +0600

    display phones done

--- js/phone.js
+++ js/phone.js
@@ -5,13 +5,37 @@ const searchButton = () => {
     const error = document.getElementById('error');
     const inputValue = input.value;
     if (isNaN(inputValue) == true) {
+        main.innerHTML = '';
         fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
             .then(res => res.json())
             .then(data => phoneDisplay(data.data))
 
         input.value = '';
+        error.innerHTML = '';
     } else {
         error.innerText = 'Please give a Phone name!';
         input.value = '';
+        main.innerHTML = '';
+    }
+};
+const phoneDisplay = (phones) => {
+    for (const phone of phones) {
+        // console.log(phone.image);
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4');
+        div.classList.add('mb-5');
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <h6 class="card-title">${phone.brand}</h6>
+                <div class="text-center">
+                    <button onclick="phoneDetails()" class="btn btn-primary ">Details</button>
+                </div>
+            </div>
+        </div>
+        `;
+        main.appendChild(div);
     }
 };
